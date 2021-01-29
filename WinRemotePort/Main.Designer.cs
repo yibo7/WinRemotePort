8934298380eb3377469718a86dc2c0adf50c6664
cqs263 <cqs263@163.com>
2021-01-29 14:09:35 +0800

    项目初始化

---
+++ WinRemotePort/Main.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WinRemotePort
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbIsAddPortToFire = new System.Windows.Forms.CheckBox();
+            this.btnSavePort = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lbTab1Info = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtNetstatInfo = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(824, 518);
+            this.tabControl1.TabIndex = 1;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.cbIsAddPortToFire);
+            this.tabPage1.Controls.Add(this.btnSavePort);
+            this.tabPage1.Controls.Add(this.txtPort);
+            this.tabPage1.Controls.Add(this.lbTab1Info);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(816, 489);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "远程桌面";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "输入要修改的远程桌面端口：";
+            // 
+            // cbIsAddPortToFire
+            // 
+            this.cbIsAddPortToFire.AutoSize = true;
+            this.cbIsAddPortToFire.Checked = true;
+            this.cbIsAddPortToFire.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbIsAddPortToFire.Location = new System.Drawing.Point(31, 152);
+            this.cbIsAddPortToFire.Margin = new System.Windows.Forms.Padding(4);
+            this.cbIsAddPortToFire.Name = "cbIsAddPortToFire";
+            this.cbIsAddPortToFire.Size = new System.Drawing.Size(269, 19);
+            this.cbIsAddPortToFire.TabIndex = 3;
+            this.cbIsAddPortToFire.Text = "是否同时向防火墙添加端口入站规则";
+            this.cbIsAddPortToFire.UseVisualStyleBackColor = true;
+            // 
+            // btnSavePort
+            // 
+            this.btnSavePort.Location = new System.Drawing.Point(31, 189);
+            this.btnSavePort.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSavePort.Name = "btnSavePort";
+            this.btnSavePort.Size = new System.Drawing.Size(100, 29);
+            this.btnSavePort.TabIndex = 2;
+            this.btnSavePort.Text = "修改端口";
+            this.btnSavePort.UseVisualStyleBackColor = true;
+            this.btnSavePort.Click += new System.EventHandler(this.btnSavePort_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(31, 113);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(85, 25);
+            this.txtPort.TabIndex = 1;
+            // 
+            // lbTab1Info
+            // 
+            this.lbTab1Info.AutoSize = true;
+            this.lbTab1Info.ForeColor = System.Drawing.Color.Red;
+            this.lbTab1Info.Location = new System.Drawing.Point(28, 21);
+            this.lbTab1Info.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTab1Info.Name = "lbTab1Info";
+            this.lbTab1Info.Size = new System.Drawing.Size(97, 15);
+            this.lbTab1Info.TabIndex = 0;
+            this.lbTab1Info.Text = "当前远程端口";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtNetstatInfo);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(816, 489);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "端口情况";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtNetstatInfo
+            // 
+            this.txtNetstatInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtNetstatInfo.Location = new System.Drawing.Point(4, 4);
+            this.txtNetstatInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNetstatInfo.Name = "txtNetstatInfo";
+            this.txtNetstatInfo.Size = new System.Drawing.Size(808, 481);
+            this.txtNetstatInfo.TabIndex = 3;
+            this.txtNetstatInfo.Text = "";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.richTextBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(816, 489);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "正在工作";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(810, 483);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.richTextBox1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(816, 489);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "活动连接";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(810, 483);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 518);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Main";
+            this.Text = "Windows远程端口修改器";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.CheckBox cbIsAddPortToFire;
+        private System.Windows.Forms.Button btnSavePort;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label lbTab1Info;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox txtNetstatInfo;
+        private System.Windows.Forms.Label label1; 
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WinRemotePort/Main.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinRemotePort
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            try
+            {
+                InitPortInfo();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("发生错误，请确认是否以管理员身份运行：" + e.Message);
+
+                this.Close();
+                this.Dispose();
+            }
+            
+        }
+
+        private void InitPortInfo()
+        {
+
+            string TerminalPort1 = RegTable.GetRegistData("PortNumber", sTcpPath);
+            string TerminalPort2 = RegTable.GetRegistData("PortNumber", sRDPPath);
+            if (!string.IsNullOrEmpty(TerminalPort1) && !string.IsNullOrEmpty(TerminalPort2))
+            {
+                if (Equals(TerminalPort1, TerminalPort2))
+                {
+                    lbTab1Info.Text = string.Format("当前远程端口为:{0}", TerminalPort1);
+                    txtPort.Text = TerminalPort1;
+                }
+                else
+                {
+                    lbTab1Info.Text = string.Format("当前远程端口tcp({0})项与RDP-Tcp({1})不相同", TerminalPort1, TerminalPort2);
+                }
+
+            }
+            else
+            {
+                lbTab1Info.Text = "找不到远程端口,请到注册表LocalMachine下，SYSTEM\\CurrentControlSet\\Control\\Terminal Server\\Wds\\rdpwd\\Tds\\tcp,确认是否存在PortNumber";
+            }
+        }
+        private string sTcpPath = "SYSTEM\\CurrentControlSet\\Control\\Terminal Server\\Wds\\rdpwd\\Tds\\tcp";
+        private string sRDPPath = "SYSTEM\\CurrentControlSet\\Control\\Terminal Server\\WinStations\\RDP-Tcp";
+        private void btnSavePort_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确定要修改端口吗？", "提示", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                int NewPort = int.Parse(txtPort.Text);
+                RegTable.SetRegistData("PortNumber", sTcpPath, NewPort);
+                RegTable.SetRegistData("PortNumber", sRDPPath, NewPort);
+                InitPortInfo();
+
+                if (cbIsAddPortToFire.Checked) //添加端口到入口规则
+                {
+                    FireWallHelp.NetFwAddPorts("远程桌面端口" + NewPort, NewPort, "TCP");
+                }
+            }
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+           
+            //
+            StreamReader reader = p.StandardOutput;//截取输出流
+            string strAllInfo = "";
+            //string strLocalInfo = "";
+            string strLine = reader.ReadLine();//每次读取一行
+            //int i = 0;
+            switch (tabControl1.SelectedIndex)
+            {
+                case 1:
+                    //查看本机端口占用情况
+                    p.StandardInput.WriteLine(" netstat -an ");
+                    p.StandardInput.WriteLine("exit");
+                    while (!reader.EndOfStream)
+                    {
+                        strAllInfo += strLine + "\r\n";
+                        //if (strLine.Trim().Length > 0)
+                        //{
+                        //    strLine = strLine.Trim();
+                        //    Regex r = new Regex(@"\s+");
+                        //    string[] strArr = r.Split(strLine);
+                        //    strLocalInfo += strArr[1] + "|\r\n";
+                        //}
+                        strLine = reader.ReadLine();//再下一行         
+                    }
+                   
+
+                    txtNetstatInfo.Text = strAllInfo;
+
+                    //this.textBox2.Text = strAllInfo;//显示 netstat -an 原信息
+                    //this.textBox3.Text = strLocalInfo;//显示 本机端口信息
+                    ////比对所查端口
+                    //if (strLocalInfo.IndexOf(":" + this.textBox1.Text + "|") >= 0)
+                    //{
+                    //    this.textBox4.Text = "已被占用";
+                    //}
+                    //else
+                    //{
+                    //    this.textBox4.Text = "可用";
+                    //}
+                    break;
+
+                case 2:
+                    //查看本机端口占用情况
+                    p.StandardInput.WriteLine(" netstat -v ");
+                    p.StandardInput.WriteLine("exit");
+                    //int i = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        strAllInfo += strLine + "\r\n";
+                        
+                        strLine = reader.ReadLine();//再下一行         
+                    }
+                     
+                     
+
+                    richTextBox2.Text = strAllInfo;
+                     
+                    break;
+                case 3:
+                    //查看本机端口占用情况
+                    p.StandardInput.WriteLine(" netstat -n ");
+                    p.StandardInput.WriteLine("exit");
+                    //int i = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        strAllInfo += strLine + "\r\n";
+
+                        strLine = reader.ReadLine();//再下一行         
+                    }
+
+
+
+                    richTextBox1.Text = strAllInfo;
+
+                    break;
+                    
+            }
+
+            p.WaitForExit();//等待程序执行完退出进程
+            p.Close();//关闭进程
+            reader.Close();//关闭流
+
+        }
+    }
+}
